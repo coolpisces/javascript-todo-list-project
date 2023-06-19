e2c5cd5710756bab7ed92df4750188ead7c74e83
coolpisces <coolpisces22@gmail.com>
2023-06-19 23:42:30 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
-// Function called while clicking add button
 function addItem() {
-
-    // Getting box and ul by selecting id;
     let item = document.getElementById("box");
     let list_item = document.getElementById("list_item");
     let alertMes = document.getElementById("alertMes");
-
     if (item.value != "") {
         hideAlert();
-        // Creating element and adding value to it
-        let make_li = document.createElement("li");
-        make_li.appendChild(document.createTextNode(item.value));
-
-        // Adding li to ul
-        list_item.appendChild(make_li);
-
-        // Reset the value of box
+        let liOlustur = document.createElement("li");
+        liOlustur.appendChild(document.createTextNode(item.value));
+        list_item.appendChild(liOlustur);
         item.value = ""
-
-        // Delete a li item on click
-        make_li.onmousedown = function () {
+        liOlustur.onmousedown = function () {
             this.parentNode.removeChild(this);
         }
-        overLi();
     }
     else {
         alertMes.style.visibility = 'visible';
