2aa8476de582bee5410be765be0d93aca89e2c97
coolpisces <coolpisces22@gmail.com>
2023-06-19 20:37:30 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // Function called while clicking add button
-function add_item() {
+function addItem() {
 
     // Getting box and ul by selecting id;
     let item = document.getElementById("box");
     let list_item = document.getElementById("list_item");
-    if (item.value != "") {
+    let alertMes = document.getElementById("alertMes");
 
+    if (item.value != "") {
+        hideAlert();
         // Creating element and adding value to it
         let make_li = document.createElement("li");
         make_li.appendChild(document.createTextNode(item.value));
@@ -20,13 +22,25 @@ function add_item() {
         make_li.onmousedown = function () {
             this.parentNode.removeChild(this);
         }
-
+        overLi();
     }
     else {
-
-
-        // Alert msg when value of box is "" empty.
-        //alert("Geçerli bir giriş yapınız!");
+        alertMes.style.visibility = 'visible';
+        alertMes.innerHTML = `    
+        <div class="alert alert-danger " role="alert">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-12">
+                Değer girmediniz!
+            <button type="button" onclick="hideAlert()" class="btn-close" data-bs-dismiss="alert"
+                aria-label="Close">
+            </button>
+            </div>
+            
+        </div>
+    </div>`;
     }
-
+}
+function hideAlert() {
+    let alertMes = document.getElementById("alertMes");
+    alertMes.style.visibility = 'hidden';
 }
